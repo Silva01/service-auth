24bb4f3967363f9cf6fb4690eedb47cba037ee49
Daniel Silva <silva20102@gmail.com>
2018-04-07 23:16:57 -0300

    Update Routes and Validations

--- api/api.rb
+++ api/api.rb
@@ -8,39 +8,27 @@ require_relative '../src/util/p_common'
 
 set :bind, '0.0.0.0'
 
-post '/service' do
+post '/' do
   content_type :json
   response.headers["Access-Control-Allow-Origin"] = "*"
 
-  validar = Validar.new
-
   servico = Service.new
 
-  method_params = JSON.parse(request.body.read.to_s)
-
-  validar.id method_params['id']
-
-  validar.jsonrpc method_params['jsonrpc']
-
-  validar.auth method_params['auth']
-
-  validar.method method_params['method']
-
-  validar.params method_params['params']
+  params = JSON.parse(request.body.read.to_s)
 
   method = servico.method(method_params['method'])
 
   if method_params['params'].class == Hash
 
     p_array = PArray.new
-    p_array.set_dados method_params
-    result = method.call p_array
+    p_array.set_dados params
+    result = servico.login p_array
 
     {:id => 'auth', :jsonrpc => '2.0', :result => result}.to_json
 
   else
     p_common = PCommon.new
-    p_common.set_dados method_params
+    p_common.set_dados params
     result = method.call p_common
 
     {:id => 'auth', :jsonrpc => '2.0', :result => result}.to_json
